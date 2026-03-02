009ebf60916d6f1f0df779abc0ff958e12d1cbdb
Benjámin <benibige2003@gmail.com>
2026-03-02 18:37:03 +0100

    FIX: Changed the dashboard app's directory handling to accommodate Gunicorn

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -3,8 +3,16 @@ import numpy as np
 
 import os
 import sys
-sys.path.append(os.path.abspath('./src/dashboard'))
-sys.path.append(os.path.abspath('./src'))
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+# Get the project root (dota-project)
+PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DIR, "../../"))
+# Get the src directory
+SRC_DIR = os.path.join(PROJECT_ROOT, "src")
+
+# Add them to sys.path
+sys.path.append(PROJECT_ROOT)
+sys.path.append(SRC_DIR)
+sys.path.append(CURRENT_DIR)
 
 from db_functions import DotaDB
 from app_functions import *
@@ -12,7 +20,8 @@ from app_functions import *
 from dash import Dash, html, dcc, Input, Output, State, callback
 import dash_mantine_components as dmc
 
-app = Dash()
+app = Dash(__name__)
+server = app.server
 app.layout = dmc.MantineProvider(
     theme={'colorScheme': 'dark', 'primaryColor': 'indigo'},
     children=[
