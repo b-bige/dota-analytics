dee6f3bbb288d99b043d2a8d8d5d112438e1ee1d
Benjámin <benibige2003@gmail.com>
2026-03-30 14:46:42 +0200

    TEST 2

--- src/scripts/run_monitor.py
+++ src/scripts/run_monitor.py
@@ -5,5 +5,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from live_match_monitor import monitor
 
+#TEST
 if __name__ == "__main__":
     monitor.run_forever(interval=60)
