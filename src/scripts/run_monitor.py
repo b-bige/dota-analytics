29854460a74e04edd6674cd11134bb8ea5bc2c1e
Benjámin <benibige2003@gmail.com>
2026-03-30 19:11:35 +0200

    ADD: Logging to run_monitor.py

--- src/scripts/run_monitor.py
+++ src/scripts/run_monitor.py
@@ -4,6 +4,9 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from live_match_monitor import monitor
+import logging
+from basic_logger import setup_logger
+listener = setup_logger(logfile_path='logs/live-match-monitor.log')
 
 #TEST
 if __name__ == "__main__":
