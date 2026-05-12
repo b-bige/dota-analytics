266c518b92ef2e93f503f26c3bc06e8fd92d1671
Benjámin <benibige2003@gmail.com>
2026-05-12 10:35:38 +0200

    FIX: Match predictor returns probability correctly as before

--- src/analytics/match_predictor.py
+++ src/analytics/match_predictor.py
@@ -1,12 +1,11 @@
 import logging
 import joblib
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
 
 class MatchPredictor:
     def __init__(self): #TODO: Add dynamic model choice
-        self._win_model = joblib.load('src/ml_models/log_reg_model.joblib')
+        self._win_model: Pipeline = joblib.load('src/ml_models/log_reg_model.joblib')
 
     def predict_win_probability(self, radiant_draft_score, dire_draft_score, avg_rad_rating, avg_dire_rating) -> float | None:
         """
@@ -20,6 +19,5 @@ class MatchPredictor:
             'rating_diff':    avg_rad_rating - avg_dire_rating,
             'draft_diff':     radiant_draft_score - dire_draft_score
         }]).fillna(0)
-
-        prob = self._win_model.predict(features.values)[0][1] 
+        prob = self._win_model.predict_proba(features.values)[0][1]
         return float(prob)
